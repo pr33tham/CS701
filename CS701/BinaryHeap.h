066a0e5d2f4125648a267043eadc549134c6ab2d
Ruthwik Preetham <rutwikritu@gmail.com>
2024-02-24 23:45:02 -0500

    Binary Heap

--- CS701/BinaryHeap.h
+++ CS701/BinaryHeap.h
@@ -2,6 +2,7 @@
 
 /*
 //to-do definition
+// fix index for 0, pseudo is for 1 
 */
 
 #include <iostream>
@@ -10,14 +11,70 @@ template <class ElementType>
 class BinaryHeap {
 public:
 	BinaryHeap(ElementType* array, size_t sizeOfArray);
-	ElementType* Sort(void);
-	~BinaryHeap(void);
+	void Sort();
 protected:
 	ElementType* data;
 	size_t SIZE;
 	inline int ParentOf(int Node);
 	inline int LeftChild(int Node);
 	inline int RightChild(int Node);
-	inline int HeapSize(void);
+	//inline int HeapSize(void);
 	void Heapify(int Node);
+	void BuildHeap(void);
 };
+
+template<class ElementType>
+BinaryHeap<ElementType>::BinaryHeap(ElementType* array, size_t sizeOfArray)
+{
+	SIZE = sizeOfArray;
+	data = array;
+	BuildHeap();
+
+}
+
+template<class ElementType>
+void BinaryHeap<ElementType>::Sort(void) {
+	BuildHeap();
+	for (int i = SIZE / 2; i > 2; i--) {
+		std::swap(data[1], data[i]);
+		SIZE--;
+		Heapify(1);
+	}
+}
+
+template<class ElementType>
+inline int BinaryHeap<ElementType>::ParentOf(int Node) {
+	return floor(Node / 2);
+}
+
+template<class ElementType>
+inline int BinaryHeap<ElementType>::LeftChild(int Node) {
+	return 2 * Node;
+}
+
+template<class ElementType>
+inline int BinaryHeap<ElementType>::RightChild(int Node) {
+	return 2 * Node + 1;
+}
+
+template<class ElementType>
+void BinaryHeap<ElementType>::Heapify(int Node) {
+	int largest = Node;				//0
+	int left = LeftChild(Node);		//1
+	int right = RightChild(Node);	//2
+
+	if (left < SIZE && data[left] > data[largest]) largest = left; //10
+	if (right < SIZE && data[right] > data[largest]) largest = right; //5
+
+	if (largest != Node) {
+		std::swap(data[Node], data[largest]);
+		Heapify(largest);
+	}
+}
+
+template<class ElementType>
+inline void BinaryHeap<ElementType>::BuildHeap(void) {
+	for (int i = SIZE / 2; i > 1; i--) {
+		Heapify(i);
+	}
+}
--- CS701/CS701.cpp
+++ CS701/CS701.cpp
@@ -2,9 +2,18 @@
 CS701.cpp : This file contains the 'main' function.
 Program execution begins and ends there.
 */
-#include <iostream>
+#include "BinaryHeap.h"
+#
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+	int array[10] = { 4, 1, 3 ,2, 16, 9, 10, 14, 8, 7};
+	size_t sizeofArray = std::size(array);
+
+	BinaryHeap<int> Heap(array, sizeofArray);
+	//Heap.Sort();
+
+	for (int i = 0; i < sizeofArray; i++) {
+		std::cout << array[i] << " ";
+	}
 }
