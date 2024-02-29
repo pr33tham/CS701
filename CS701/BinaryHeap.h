5b140388352b76f86549a332de7e00491125c215
Ruthwik Preetham <rutwikritu@gmail.com>
2024-02-29 18:58:00 -0500

    Binary Heap Index Fix
    Fixed indexing - originally with index 1 to 0.

--- CS701/BinaryHeap.h
+++ CS701/BinaryHeap.h
@@ -1,8 +1,8 @@
 #pragma once
 
 /*
-//to-do definition
-// fix index for 0, pseudo is for 1 
+1. to-do definition
+2. fix index for 0, pseudo is for 1
 */
 
 #include <iostream>
@@ -35,36 +35,36 @@ BinaryHeap<ElementType>::BinaryHeap(ElementType* array, size_t sizeOfArray)
 template<class ElementType>
 void BinaryHeap<ElementType>::Sort(void) {
 	BuildHeap();
-	for (int i = SIZE / 2; i > 2; i--) {
-		std::swap(data[1], data[i]);
+	for (int i = SIZE / 2 - 1; i > 0; i--) {
+		std::swap(data[0], data[i]);
 		SIZE--;
-		Heapify(1);
+		Heapify(0);
 	}
 }
 
 template<class ElementType>
 inline int BinaryHeap<ElementType>::ParentOf(int Node) {
-	return floor(Node / 2);
+	return (Node - 1) / 2;
 }
 
 template<class ElementType>
 inline int BinaryHeap<ElementType>::LeftChild(int Node) {
-	return 2 * Node;
-}
-
-template<class ElementType>
-inline int BinaryHeap<ElementType>::RightChild(int Node) {
 	return 2 * Node + 1;
 }
 
 template<class ElementType>
-void BinaryHeap<ElementType>::Heapify(int Node) {
-	int largest = Node;				//0
-	int left = LeftChild(Node);		//1
-	int right = RightChild(Node);	//2
+inline int BinaryHeap<ElementType>::RightChild(int Node) {
+	return 2 * Node + 2;
+}
 
-	if (left < SIZE && data[left] > data[largest]) largest = left; //10
-	if (right < SIZE && data[right] > data[largest]) largest = right; //5
+template<class ElementType>
+void BinaryHeap<ElementType>::Heapify(int Node) {
+	int largest = Node;
+	int left = LeftChild(Node);
+	int right = RightChild(Node);
+
+	if (left < SIZE && data[left] > data[largest]) largest = left;
+	if (right < SIZE && data[right] > data[largest]) largest = right;
 
 	if (largest != Node) {
 		std::swap(data[Node], data[largest]);
@@ -74,7 +74,7 @@ void BinaryHeap<ElementType>::Heapify(int Node) {
 
 template<class ElementType>
 inline void BinaryHeap<ElementType>::BuildHeap(void) {
-	for (int i = SIZE / 2; i > 1; i--) {
+	for (int i = SIZE / 2 - 1; i >= 0; i--) {
 		Heapify(i);
 	}
 }
