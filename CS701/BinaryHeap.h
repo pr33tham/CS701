0353219318050cbf37d634ed147ae1c7fa590b3d
Ruthwik Preetham <rutwikritu@gmail.com>
2024-03-13 19:15:48 -0400

    Update BinaryHeap.h

--- CS701/BinaryHeap.h
+++ CS701/BinaryHeap.h
@@ -10,21 +10,19 @@
 template <class ElementType>
 class BinaryHeap {
 public:
-	BinaryHeap(int maxSize = 10 ^ 6);
-	BinaryHeap(ElementType* array, size_t sizeOfArray, int maxSize = 10 ^ 6);
+	BinaryHeap(int maxSize = pow(10, 6));
+	BinaryHeap(ElementType* array, size_t sizeOfArray, int maxSize = pow(10, 6));
 	//ElementType* Sort(void);
 	~BinaryHeap(void);
-	void Add(ElementType &Item);
-	void Remove();
-protected:
+	inline void Insert(ElementType& newElement);
+	inline ElementType Remove(void);
 	ElementType* data;
+protected:
 	size_t currentSizeOfHeap;
 	const int MAXSIZE;
 	inline int ParentOf(int Node);
 	inline int LeftChild(int Node);
 	inline int RightChild(int Node);
-	inline void Insert(ElementType &newElement);
-	inline void Remove(void);
 	inline size_t HeapSize(void);
 	void Heapify(int Node);
 	void BuildHeap(void);
@@ -48,35 +46,23 @@ inline BinaryHeap<ElementType>::BinaryHeap(ElementType* array, size_t sizeOfArra
 	BuildHeap();
 }
 
-//template<class ElementType>
-//ElementType* BinaryHeap<ElementType>::Sort(void) {
-//	size_t SIZE = currentSizeOfHeap;
-//	BuildHeap();
-//	for (int i = SIZE / 2 - 1; i > 0; i--) {
-//		std::swap(data[0], data[i]);
-//		SIZE--;
-//		Heapify(0);
-//	}
-//	return data;
-//}
-
 template<class ElementType>
 inline BinaryHeap<ElementType>::~BinaryHeap(void) {
 	if (data) delete[] data;
 }
 
 template<class ElementType>
-inline void BinaryHeap<ElementType>::Add(ElementType& Item) {
+inline void BinaryHeap<ElementType>::Insert(ElementType& Item) {
 	if (currentSizeOfHeap >= MAXSIZE) {
 		std::cerr << "Heap overflow. Increase the size of heap" << std::endl;
 		std::exit(EXIT_FAILURE);
 	}
-	Data[currentSizeOfHeap] = Item;
+	data[currentSizeOfHeap] = Item;
 	Heapify(currentSizeOfHeap++);
 ;}
 
 template<class ElementType>
-inline void BinaryHeap<ElementType>::Remove() {
+inline ElementType BinaryHeap<ElementType>::Remove() {
 	if (currentSizeOfHeap <= 0) {
 		std::cerr << "Heap underflow!" << std::endl;
 		std::exit(EXIT_FAILURE);
@@ -85,7 +71,6 @@ inline void BinaryHeap<ElementType>::Remove() {
 	data[0] = data[--currentSizeOfHeap];
 	Heapify(0);
 	return top;
-
 }
 
 template<class ElementType>
@@ -101,10 +86,6 @@ inline int BinaryHeap<ElementType>::LeftChild(int Node) {
 template<class ElementType>
 inline int BinaryHeap<ElementType>::RightChild(int Node) {
 	return 2 * Node + 2;
-}
-
-template<class ElementType>
-inline void BinaryHeap<ElementType>::Insert(ElementType& newElement) {
 }
 
 template<class ElementType>
@@ -130,7 +111,6 @@ void BinaryHeap<ElementType>::Heapify(int Node) {
 
 template<class ElementType>
 inline void BinaryHeap<ElementType>::BuildHeap(void) {
-	currentSizeOfHeap = currentSizeOfHeap;
 	for (int i = currentSizeOfHeap / 2 - 1; i >= 0; i--) {
 		Heapify(i);
 	}
