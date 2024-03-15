6dd8f9e85b2a96c8cb2890d5d0441341f4e7de7b
Ruthwik Preetham <ruthwikpr33tham@gmail.com>
2024-03-14 20:14:56 -0400

    Update comment header template

--- CS701/BinaryHeapPriorityQueue.h
+++ CS701/BinaryHeapPriorityQueue.h
@@ -4,23 +4,56 @@
 #include <iostream>
 #include "PriorityQueueElement.h"
 
-/*
-* Include Header File from template
-* to-do comments
-* fix bugs
-*/
+/*************************************************************************
+ *
+ *  Pace University
+ *  Spring 2024
+ *  Advanced Algorithms
+ *
+ *  Course: CS 701-801
+ *  Name: RUTHWIK PREETHAM, SURAPARAJU
+ *  Collaborators: NONE
+ *  References: INTRO TO ALGORITHMS, 3RD EDITION; CPROGRAMMING.COM; 
+ *
+ *  Assignment: #1
+ *  Problem: Write two generic classes: BinaryHeapPriorityQueue<T> and vEBtreePriorityQueue<T>, with methods ExtractMax(), Insert(value,priority), 
+ *	IncreaseKey(value,priority), and a constructor to create the data structure and populate it with a parametric collection of n items. (You may 
+ *	include other methods if you want, but these are the required.) Your methods must implement the pseudocode included in our textbook (3rd edition, 
+ *	Chapters 6 and 20). No points for code implementing other algorithms. Description: PUT A BRIEF DESCRIPTION HERE
+ *  Input: 
+ *  Output: 
+ *
+ *  Visible data fields:
+ *	size_t SIZE;										//Current number of elements
+ *
+ *  Visible methods:
+ *  BinaryHeapPriorityQueue(int maxSize = pow(10, 6));	//Constructor
+ *	ElementType ExtractMax();							//Returns and removes the max prioprity element
+ *	void Insert(ElementType value, int priority);		//Inserts element to the heap
+ *	void IncreaseKey(int index, int newPriority);		//Increases priority 
+ *	~BinaryHeapPriorityQueue();							//Deletes the heap array
+ *
+ *
+ *   Remarks
+ *   -------
+ *
+ *   PUT ALL NON-CODING ANSWERS HERE
+ *	 
+ * 
+ *
+ *************************************************************************/
 
 template<class ElementType> 
 class BinaryHeapPriorityQueue {
 public:
-	BinaryHeapPriorityQueue(int maxSize = pow(10, 6));	
-	ElementType ExtractMax();						//Returns and removes the max prioprity element
-	void Insert(ElementType value, int priority);	//Inserts element to the heap
-	void IncreaseKey(int index, int newPriority);	//Increases priority 
-	~BinaryHeapPriorityQueue();						//Deletes the heap array
-	size_t SIZE;									//Current number of elements
-	PriorityQueueElement<ElementType>* data;		//Data array
+	BinaryHeapPriorityQueue(int maxSize = pow(10, 6));	//Constructor
+	ElementType ExtractMax();							//Returns and removes the max prioprity element
+	void Insert(ElementType value, int priority);		//Inserts element to the heap
+	void IncreaseKey(int index, int newPriority);		//Increases priority 
+	~BinaryHeapPriorityQueue();							//Deletes the heap array
+	size_t SIZE;										//Current number of elements
 private:
+	PriorityQueueElement<ElementType>* data;		    //Data array
 	const int MAXSIZE;					//Maximum number of elements
 	inline int ParentOf(int Node);		//Returns parents location
 	inline int LeftChild(int Node);		//Returns left child location
--- CS701/PriorityQueueElement.h
+++ CS701/PriorityQueueElement.h
@@ -1,6 +1,45 @@
 #ifndef PRIORITYQUEUEELEMENT_H
 #define PRIORITYQUEUEELEMENT_H
 
+/*************************************************************************
+ *
+ *  Pace University
+ *  Spring 2024
+ *  Advanced Algorithms
+ *
+ *  Course: CS 701-801
+ *  Name: RUTHWIK PREETHAM, SURAPARAJU
+ *  Collaborators: NONE
+ *  References: INTRO TO ALGORITHMS, 3RD EDITION; CPROGRAMMING.COM;
+ *
+ *  Assignment: #1
+ *  Problem: Write two generic classes: BinaryHeapPriorityQueue<T> and vEBtreePriorityQueue<T>, with methods ExtractMax(), Insert(value,priority),
+ *	IncreaseKey(value,priority), and a constructor to create the data structure and populate it with a parametric collection of n items. (You may
+ *	include other methods if you want, but these are the required.) Your methods must implement the pseudocode included in our textbook (3rd edition,
+ *	Chapters 6 and 20). No points for code implementing other algorithms. Description: PUT A BRIEF DESCRIPTION HERE
+ *  Input: 
+ *  Output:
+ *
+ *  Visible data fields:
+ *	size_t SIZE;										//Current number of elements
+ *
+ *  Visible methods:
+ *  BinaryHeapPriorityQueue(int maxSize = pow(10, 6));
+ *	ElementType ExtractMax();							//Returns and removes the max prioprity element
+ *	void Insert(ElementType value, int priority);		//Inserts element to the heap
+ *	void IncreaseKey(int index, int newPriority);		//Increases priority
+ *	~BinaryHeapPriorityQueue();							//Deletes the heap array
+ *
+ *
+ *   Remarks
+ *   -------
+ *
+ *   PUT ALL NON-CODING ANSWERS HERE
+ *
+ *
+ *
+ *************************************************************************/
+
 template<typename ValueType>
 struct PriorityQueueElement {
     ValueType value;
