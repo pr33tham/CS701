da042e3ce3b525403b15982226ced7bd8fccaf67
Ruthwik Preetham <ruthwikpr33tham@gmail.com>
2024-03-21 18:46:10 -0400

    header file changes

--- CS701/PriorityQueueElement.h
+++ CS701/PriorityQueueElement.h
@@ -1,38 +1,6 @@
 #ifndef PRIORITYQUEUEELEMENT_H
 #define PRIORITYQUEUEELEMENT_H
 
-/*************************************************************************
- *
- *  Pace University
- *  Spring 2024
- *  Advanced Algorithms
- *
- *  Course: CS 701-801
- *  Name: RUTHWIK PREETHAM, SURAPARAJU
- *  Collaborators: NONE
- *  References: NONE
- *
- *  Assignment: #1
- *  Problem: Not in context with any problem
- *  Input: 
- *  Output:
- *
- *  Visible data fields:
- *  ValueType value;    //Actual value for queue
- *  int priority;       //Priotity of the value
- *
- *  Visible methods:
- *  NONE
- *
- *   Remarks
- *   -------
- *
- *   PUT ALL NON-CODING ANSWERS HERE
- *
- *
- *
- *************************************************************************/
-
 template<typename ValueType>
 struct PriorityQueueElement {
     ValueType value;    //Actual value for queue
